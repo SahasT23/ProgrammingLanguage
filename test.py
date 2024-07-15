674515ba3f3522a36fc920aee11cba1d641edf39
SahasT23 <sahas.st01@gmail.com>
2024-07-15 20:15:23 +0100

    Created test.py, don't know if I will still do this in python, but it will be a new experience

---
+++ test.py
@@ -0,0 +1,21 @@
+# focus on memory
+# not sure if I want to do this in python or another language
+# lexical analysis
+# parsing
+# OOP -> look at functional programming later
+# Encapsulation
+# Abstraction
+# Inheritance
+# Polymorphism
+# Class variables
+# Class methods
+# Static methods
+# Inheritance py
+# Data structures
+# Stacks and Heaps
+# Interpreter
+# Recursion
+# Building lexer, parser and interpreter
+# Variables
+# Operators
+# Conditional statements
